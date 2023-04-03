0ca6ddd89f18fa750329d1ebc9e475be6d8ecb0d
Nobime Romeo <nobimeromeo@pop-os.localdomain>
2023-04-03 15:16:46 +0200

    added models and views of skills, projects. modified header

--- website/models.py
+++ website/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Skill(models.Model):
+    name = models.CharField(max_length=50)
+    years_of_experience = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+
+class Project(models.Model):
+    image = models.CharField(max_length=1000)
+    name = models.CharField(max_length=50)
+    skills = models.ManyToManyField("Skill")
+    project_link = models.CharField(max_length=1000)
+    code_link = models.CharField(max_length=1000)
+
+    def __str__(self):
+        return self.name
--- website/views.py
+++ website/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render, redirect
 from django.views.generic import TemplateView
+from .models import Skill, Project
 
 
 class IndexView(TemplateView):
     """Display index"""
 
     template_name = "website/index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["skills"] = Skill.objects.all()
+        context["projects"] = Project.objects.all()
+        return context
